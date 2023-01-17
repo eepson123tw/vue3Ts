6a57a204fcfe38c7868e748dcfe3d0f3df157aad
allen <eepson123@gmail.com>
2023-01-18 00:12:32 +0800

    feat:Add three.js && squre

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,30 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" :src="logo" />
-    <HelloWorld @wow="sayWow" msg="Hello Vue 3 + TypeScript + Vite" />
-  </div>
+  <div class="index" ref="bg"></div>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from '@cp/HelloWorld.vue'
-import logo from '@img/logo.png'
+import { ref, onMounted } from 'vue'
+import * as THREE from 'three'
 
-const sayWow = () => {
-  console.log('parent wow')
-}
+const bg = ref<HTMLInputElement | null>(null)
+
+onMounted(() => {
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  bg.value && bg.value.appendChild(renderer.domElement)
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
+  camera.position.z = 5
+
+  function animate() {
+    requestAnimationFrame(animate)
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    renderer.render(scene, camera)
+  }
+  animate()
+})
 </script>
 
 <style lang="less" scoped>
-.home {
+.index {
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: @color-black;
-  padding: (@margin * 3) 0;
   margin: 0 auto;
+  // background: black;
 }
 </style>
